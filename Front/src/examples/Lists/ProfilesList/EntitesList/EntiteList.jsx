dfcf8877feefc7d02f6ae3e1ef159fdc9b678771
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-10-22 12:23:18 +0200

    ..

--- Front/src/examples/Lists/ProfilesList/EntitesList/EntiteList.jsx
+++ Front/src/examples/Lists/ProfilesList/EntitesList/EntiteList.jsx
@@ -1,3 +1,15 @@
+/**
+ * This component renders a list of entities.
+ *
+ * It fetches the list of entities from the backend when it mounts.
+ * It renders a card for each entity, with a button to edit the entity.
+ * It also renders a button to add a new entity, and a modal to edit or add an entity.
+ *
+ * The modal is used to edit or add an entity. It contains a form with the entity's name, email, and phone.
+ * When the form is submitted, it sends the data to the backend and then fetches the new list of entities.
+ *
+ * If there is an error, it renders an alert with the error message.
+ */
 import React, { useEffect, useState } from 'react';
 import entityService from 'services/entityService';
 // @mui material components
@@ -12,8 +24,9 @@ import MDInput from 'components/MDInput';
 import MDAlert from 'components/MDAlert';
 import EntiteCard from 'examples/Cards/EntiteCard/EntiteCard';
 import EntiteModal from 'examples/popup/EntitePopUp/EntitePopUp';
+
 const EntiteList = () => {
-  const [entites, setentites] = useState([]);
+  const [entites, setEntites] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedEntity, setSelectedEntity] = useState(null);
   const [alert, setAlert] = useState({ show: false, message: '', type: '' });
@@ -25,38 +38,90 @@ const EntiteList = () => {
     code_postal: '',
     role: '',
   });
+
   useEffect(() => {
-    fetchActiveentites();
+    fetchActiveEntites();
   }, []);
-  // const fetchentites = async () => {
-  //   const result = await entityService.getAllEntities();
-  //   if (result.success) {
-  //     setentites(result.data);
-  //   } else {
-  //     console.error(result.error);
-  //   }
-  // };
-  const handleAddEntite = () => {
-    setSelectedEntity(null); // Clear selected contact for new entry
-    setShowModal(true); // Show modal for adding a new contact
+
+  // Function to render the search results
+  const renderSearch = () => {
+    if (
+      searchQuery.nom.length > 0 ||
+      searchQuery.ville.length > 0 ||
+      searchQuery.region.length > 0 ||
+      searchQuery.code_postal.length > 0 ||
+      searchQuery.role.length > 0
+    ) {
+      const filteredEntites = entites.filter(entite => {
+        const nom = entite.nom
+          ? entite.nom.toLowerCase().includes(searchQuery.nom.toLowerCase())
+          : false;
+        const ville = entite.ville
+          ? entite.ville.toLowerCase().includes(searchQuery.ville.toLowerCase())
+          : false;
+        const region = entite.region
+          ? entite.region.toLowerCase().includes(searchQuery.region.toLowerCase())
+          : false;
+        const code_postal = entite.code_postal
+          ? entite.code_postal.toLowerCase().includes(searchQuery.code_postal.toLowerCase())
+          : false;
+        const role = entite.role
+          ? entite.role.toLowerCase().includes(searchQuery.role.toLowerCase())
+          : false;
+        return nom || ville || region || code_postal || role;
+      });
+
+      return filteredEntites; // Return filtered entities
+    }
+    return entites; // Return original entities if no search query
   };
+
+  // Fetch Active and Inactive entities
+  const fetchActiveEntites = async () => {
+    const result = await entityService.getActiveEntites();
+    if (result.success) {
+      setEntites(result.data); // Update your entites state here
+    } else {
+      console.error(result.error);
+    }
+  };
+
+  const fetchInactiveEntites = async () => {
+    const result = await entityService.getInactiveEntites();
+    if (result.success) {
+      setEntites(result.data); // Update your entites state here
+    } else {
+      console.error(result.error);
+    }
+  };
+
+  // Function to handle adding entities
+  const handleAddEntite = () => {
+    setSelectedEntity(null); // Clear selected entity for new entry
+    setShowModal(true); // Show modal for adding a new entity
+  };
+
+  // Function to close modal
   const handleModalClose = () => {
     setShowModal(false); // Hide modal
-    fetchActiveentites(); // Refresh contact list after adding/editing
+    fetchActiveEntites(); // Refresh entity list after adding/editing
   };
+
+  // Function to handle saving entity
   const handleSave = async data => {
     let result;
     let successMessage = '';
     if (selectedEntity) {
-      // Update contact
+      // Update entity
       console.log('Updating entity:', selectedEntity.Eid);
       result = await entityService.updateEntity(selectedEntity.Eid, data);
-      successMessage = 'entite updated successfully!';
+      successMessage = 'Entité updated successfully!';
     } else {
-      // Create new contact
+      // Create new entity
       result = await entityService.createEntity(data);
-      successMessage = 'entite saved successfully!';
+      successMessage = 'Entité saved successfully!';
     }
+
     // Handle the result with alert feedback
     if (result.success) {
       setAlert({ show: true, message: successMessage, type: 'success' });
@@ -65,72 +130,69 @@ const EntiteList = () => {
     }
     handleModalClose();
     if (isActive) {
-      fetchActiveentites(); // If isActive is true, fetch active entities
+      fetchActiveEntites(); // Fetch active entities
     } else {
-      fetchInactiveentites(); // If isActive is false, fetch inactive entities
+      fetchInactiveEntites(); // Fetch inactive entities
     }
     setIsActive(true); // Set the switch state to Active after modifying an entity
   };
+
   // Function to close the alert
   const handleCloseAlert = () => {
     setAlert({ show: false, message: '', type: '' });
   };
-  /////////////////////////////////////////////////////////////////////////////////SEARCH
-  // const handleSearchChange = e => {
-  //   const { name, value } = e.target;
-  //   setSearchQuery(prev => ({ ...prev, [name]: value }));
 
-  //   if (value === '') {
-  //     fetchActiveentites(); // If search input is cleared, fetch active entities
-  //   } else {
-  //     handleSearchEntities(); // If there's input, fetch filtered entities
-  //   }
-  // };
+  // Search functionality
+  const handleSearchChange = e => {
+    const { name, value } = e.target;
+    setSearchQuery(prev => ({ ...prev, [name]: value }));
+    if (value === '') {
+      fetchActiveEntites(); // If search input is cleared, fetch active entities
+    } else {
+      handleSearchEntities(); // If there's input, fetch filtered entities
+    }
+  };
+
   const handleSearchEntities = async () => {
-    const result = await entityService.searchEntities(searchQuery);
-    if (result.success) {
-      setentites(result.data);
-    } else {
-      console.error(result.error);
+    try {
+      if (!searchQuery || typeof searchQuery !== 'object') {
+        console.error('Invalid searchQuery: searchQuery must be an object');
+        return;
+      }
+      const result = await entityService.searchEntities(searchQuery);
+      if (result.success) {
+        setEntites(result.data);
+      } else {
+        console.error(result.error);
+      }
+    } catch (error) {
+      console.error('Error searching for entities:', error);
     }
   };
-  ///////////////////////////////////////////////////////////////////////// TOGGLE ACTIVE / Inactive
-  const fetchActiveentites = async () => {
-    const result = await entityService.getActiveEntites();
-    if (result.success) {
-      setentites(result.data); // Update your entites state here
-    } else {
-      console.error(result.error);
-    }
-  };
-  const fetchInactiveentites = async () => {
-    const result = await entityService.getInactiveEntites();
-    if (result.success) {
-      setentites(result.data); // Update your entites state here
-    } else {
-      console.error(result.error);
-    }
-  };
-  // GetActive and Inactive entites
+
+  // Toggle active/inactive entities
   const handleToggleActiveInactive = async () => {
     setIsActive(prevIsActive => {
       const newIsActive = !prevIsActive; // Toggle the active state
-      // Fetch entites based on the new state
+      // Fetch entities based on the new state
       if (newIsActive) {
-        fetchActiveentites();
+        fetchActiveEntites();
       } else {
-        fetchInactiveentites();
+        fetchInactiveEntites();
       }
       return newIsActive; // Update the state
     });
   };
-  ////////////////////////////////////////////////////////////////////////
+
+  // Render filtered entities
+  const filteredEntites = renderSearch();
+
   return (
     <div className="entite-list">
       <Card id="delete-account">
         <MDBox pt={2} px={2} display="flex" justifyContent="space-between" alignItems="center">
           <MDTypography variant="h6" fontWeight="medium">
-            Entites
+            Entités
           </MDTypography>
           <MDBox pr={1}>
             <div className="entite-list">
@@ -138,16 +200,13 @@ const EntiteList = () => {
                 label="Search"
                 name="nom"
                 value={searchQuery.nom}
-                onChange={e => {
-                  setSearchQuery({ ...searchQuery, nom: e.target.value });
-                  handleSearchEntities();
-                }}
+                onChange={handleSearchChange}
                 style={{ marginBottom: '10px', marginRight: '10px' }}
               />
               <MDButton
                 onClick={() => {
                   setSearchQuery({ nom: '', ville: '', region: '', code_postal: '', role: '' });
-                  fetchActiveentites(); // Reset to active entities
+                  fetchActiveEntites();
                 }}
                 variant="gradient"
                 color="dark"
@@ -158,7 +217,7 @@ const EntiteList = () => {
           </MDBox>
           <MDButton onClick={handleAddEntite} variant="gradient" color="dark">
             <Icon sx={{ fontWeight: 'bold' }}>add</Icon>
-            &nbsp;Ajouter Entite
+            &nbsp;Ajouter Entité
           </MDButton>
         </MDBox>
         <MDBox p={2}>
@@ -170,18 +229,15 @@ const EntiteList = () => {
             checked={isActive}
             onChange={handleToggleActiveInactive}
             style={{ marginRight: '10px' }}
-          >
-            {' '}
-            {isActive ? 'Active' : 'Inactive'}
-          </Switch>
+          />
           <Grid container spacing={3}>
-            {entites.map(entite => (
+            {filteredEntites.map(entite => (
               <Grid item xs={12} sm={8} md={4} key={entite.id}>
                 <EntiteCard
                   entite={entite}
                   onEdit={() => {
-                    setSelectedEntity(entite); // Set selected contact
-                    setShowModal(true); // Show modal for editing
+                    setSelectedEntity(entite);
+                    setShowModal(true);
                   }}
                 />
               </Grid>
