ff813fea313d14092504eecd063bdc1c1c72acbb
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-10-22 15:27:03 +0200

    ..

--- Front/src/layouts/dashboard/index.js
+++ Front/src/layouts/dashboard/index.js
@@ -1,9 +1,7 @@
 // @mui material components
 import Grid from '@mui/material/Grid';
-
 // Material Dashboard 2 React components
 import MDBox from 'components/MDBox';
-
 // Material Dashboard 2 React example components
 import DashboardLayout from 'examples/LayoutContainers/DashboardLayout';
 import DashboardNavbar from 'examples/Navbars/DashboardNavbar';
@@ -11,7 +9,6 @@ import Footer from 'examples/Footer';
 import ReportsBarChart from 'examples/Charts/BarCharts/ReportsBarChart';
 import ReportsLineChart from 'examples/Charts/LineCharts/ReportsLineChart';
 import ComplexStatisticsCard from 'examples/Cards/StatisticsCards/ComplexStatisticsCard';
-
 // Data
 import reportsBarChartData from 'layouts/dashboard/data/reportsBarChartData';
 import reportsLineChartData from 'layouts/dashboard/data/reportsLineChartData';
