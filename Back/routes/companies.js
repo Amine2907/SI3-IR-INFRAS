4d26ba31980ea83e299eefb2f27f44ce42805ecc
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-10-24 19:37:26 +0200

    ..

--- Back/routes/companies.js
+++ Back/routes/companies.js
@@ -1,3 +1,16 @@
+/**
+ * This file contains the routes for the Companys feature.
+ * It provides endpoints for creating, getting, and updating Companys.
+ * The endpoints are:
+ * - POST /api/companies: creates a new Company
+ * - GET /api/companies: gets all the Companys
+ * - GET /api/companies/active: gets all the active Companys
+ * - GET /api/companies/inactive: gets all the inactive Companys
+ * - GET /api/companies/:id: gets a Company by its ID
+ * - PUT /api/companies/:id: updates a Company
+ * - PATCH /api/companies/:id/desactivate: deactivates a Company
+ * - PATCH /api/companies/:id/activate: activates a Company
+ */
 import express from "express";
 const router = express.Router();
 import companyController from "../controllers/companyController.js";
