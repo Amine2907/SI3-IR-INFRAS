cf31b2f74b3cd41ba57c9e8d5af732c6135ca1b6
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2024-12-06 14:59:40 +0100

    ..

--- Front/src/layouts/site_details/Components/Propect/ProspectHeader/index.jsx
+++ Front/src/layouts/site_details/Components/Propect/ProspectHeader/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import Icon from '@mui/material/Icon';
@@ -10,26 +11,59 @@ import ProspectModal from 'examples/popup/ProspectsPopUp/ProspectPopUp';
 import SiteProspectService from 'services/site_details/Prospect/prospectService';
 import MDAlert from 'components/MDAlert';
 import CombinedStorageModal from 'examples/popup/ProspectDpStoragePopUp/CombinedSPopUp';
+
 function Pheader() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [showUploadModal, setshowUploadModal] = useState(false);
+  const [prospectData, setLocalProspectData] = useState(null);
   const [selectedprospect, setSelectedprospect] = useState(null);
   const [selecteddp, setSelecteddp] = useState(null);
   const [alert, setAlert] = useState(false);
+  const [prospectsData, setProspectsData] = useState([]); // State to hold prospects data
   const location = useLocation();
   const { EB } = location.state || {};
   const Sid = EB;
+
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
   const handleUpload = async () => {
     setshowUploadModal(true);
     setIsModalOpen(true);
   };
+
   const handleAddProspect = () => {
     setShowModal(true);
   };
+  const fetchProspectsData = useCallback(async () => {
+    if (Sid) {
+      try {
+        setLoading(true);
+        const response = await SiteProspectService.getProspectsSite(Sid);
+        if (response.success && response.data) {
+          setLocalUserData(response.data);
+        } else {
+          setError(response.error?.message || 'Échec de la récupération des données utilisateur.');
+        }
+      } catch (err) {
+        setError('An error occurred while fetching user data: ' + err.message);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
+      setError('Les informations utilisateur ne sont pas disponibles');
+    }
+  }, []);
+  // Fetch the prospects data whenever the Sid changes
+  useEffect(() => {
+    fetchProspectsData();
+  }, [Sid]);
+
   const handleSave = async data => {
     const { prospectData } = data;
     try {
@@ -39,6 +73,8 @@ function Pheader() {
       if (result.success) {
         successMessage = 'Prospect enregistré avec succès !';
         setAlert({ show: true, message: successMessage, type: 'success' });
+        // After successfully creating the prospect, fetch the updated list of prospects
+        fetchProspectsData(); // Refetch the data to include the newly created prospect
       } else {
         let errorMessage = `Error: ${result.error}`;
         // Check if the error is related to a prospect already existing with status 'Prospect Validé'
