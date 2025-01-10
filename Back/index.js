83dd999548b2c33f8120f0f4e6aa4908e92ef6ba
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2025-01-11 00:49:18 +0100

    syc

--- Back/index.js
+++ Back/index.js
@@ -80,6 +80,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 // Start Server
-app.listen(5000, () => {
-    console.log('Backend running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
 });
