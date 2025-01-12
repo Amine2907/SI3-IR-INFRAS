3f0ec25df6f8b0d362a77b12f0d21a37db357a06
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2025-01-12 20:09:40 +0100

    ..

--- Front/src/services/Auth/authService.js
+++ Front/src/services/Auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://si3-ir-infras.onrender.com/api/auth';
 // Sign in the user
 const signIn = async (email, password) => {
   return await axios
