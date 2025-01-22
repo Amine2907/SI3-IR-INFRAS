c7141738627558e8ea4b31377ae6dbe2d10f5e0d
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2025-01-22 10:15:19 +0100

    Reporting Architecture

---
+++ Front/src/layouts/reporting_global/index.jsx
@@ -0,0 +1,12 @@
+import DashboardLayout from 'examples/LayoutContainers/DashboardLayout';
+import Footer from 'examples/Footer';
+import DashboardNavbar from 'examples/Navbars/DashboardNavbar';
+function ReportingGlobal() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <Footer />
+    </DashboardLayout>
+  );
+}
+export default ReportingGlobal;
