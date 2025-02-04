7e3b9eba8042af6755cbdb52a54e6fe58d3ccfdf
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2025-02-04 14:18:42 +0100

    Task 37 Part 2

--- Front/src/examples/popup/DeclPreaPopUp/DpPopUp.jsx
+++ Front/src/examples/popup/DeclPreaPopUp/DpPopUp.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import PropTypes from 'prop-types';
 import MDButton from 'components/MDButton';
 import MDInput from 'components/MDInput';
 import MDTypography from 'components/MDTypography';
-import { Switch, Select, MenuItem, FormControl, Grid, FormHelperText } from '@mui/material';
+import { Switch, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
@@ -70,257 +70,216 @@ const DpUModal = ({ dp, onSave, onClose }) => {
         <MDTypography variant="h3" fontWeight="medium" textAlign="center">
           Modifier DP
         </MDTypography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Etat Prerequis</MDTypography>
-            <FormControl
-              fullWidth
-              style={{
-                marginTop: '4px',
-                marginBottom: '2px',
-              }}
-              required
-            >
-              <Select
-                name="etat_prerequis"
-                value={formData.etat_prerequis || ''}
-                onChange={handleChange}
-                displayEmpty
-                style={{
-                  padding: '10px',
-                  fontSize: '14px',
-                  borderColor: errors.etat_prerequis ? 'red' : '',
-                }}
-                required
-              >
-                <MenuItem value="" disabled>
-                  -- Choisir l&apos;etat prerequis --
-                </MenuItem>
-                <MenuItem value="Complet">Complet</MenuItem>
-                <MenuItem value="Incomplet">Incomplet</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Numero DP</MDTypography>
-            <MDInput
-              name="numero_DP"
-              value={formData.numero_DP || ''}
+        <div className={styles.formGrid}>
+          <FormControl
+            fullWidth
+            style={{
+              marginTop: '12px',
+              marginBottom: '2px',
+              width: '320px',
+            }}
+            required
+          >
+            <Select
+              name="etat_prerequis"
+              value={formData.etat_prerequis || ''}
               onChange={handleChange}
-              placeholder="Numero DP"
+              displayEmpty
               style={{
-                marginTop: '4px',
-                width: '100%',
-                borderColor: errors.numero_DP ? 'red' : '',
-              }}
-              required
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">ANO Certificat Tacite</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="ANO_certificat_tacite"
-                value={
-                  formData.ANO_certificat_tacite ? dayjs(formData.ANO_certificat_tacite) : null
-                }
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'ANO_certificat_tacite',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Arrete Opposition</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="arrete_opposition"
-                value={formData.arrete_opposition ? dayjs(formData.arrete_opposition) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'arrete_opposition',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Derniere Verification</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="derniere_verification"
-                value={
-                  formData.derniere_verification ? dayjs(formData.derniere_verification) : null
-                }
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'derniere_verification',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">MJS</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="MJS"
-                value={formData.MJS ? dayjs(formData.MJS) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'MJS',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Plans</MDTypography>
-            <FormControl
-              fullWidth
-              style={{
-                marginTop: '4px',
-                marginBottom: '2px',
+                padding: '10px',
+                fontSize: '14px',
+                borderColor: errors.etat_prerequis ? 'red' : '',
               }}
               required
             >
-              <Select
-                name="plans"
-                value={formData.plans || ''}
-                onChange={handleChange}
-                displayEmpty
-                style={{
-                  padding: '10px',
-                  fontSize: '14px',
-                  borderColor: errors.plans ? 'red' : '',
-                }}
-                required
-              >
-                <MenuItem value="" disabled>
-                  -- Choisir le status du plan --
-                </MenuItem>
-                <MenuItem value="OK">OK</MenuItem>
-                <MenuItem value="NOK">NOK</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Production DP PC</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="production_DP_PC"
-                value={formData.production_DP_PC ? dayjs(formData.production_DP_PC) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'production_DP_PC',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Recipisse Depot DP</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="recipisse_depot_DP"
-                value={formData.recipisse_depot_DP ? dayjs(formData.recipisse_depot_DP) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'recipisse_depot_DP',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Status Go Traveaux (P)</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="status_go_traveauxP"
-                value={formData.status_go_traveauxP ? dayjs(formData.status_go_traveauxP) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'status_go_traveauxP',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Status Go Traveaux (R)</MDTypography>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DesktopDatePicker
-                name="status_go_traveauxR"
-                value={formData.status_go_traveauxR ? dayjs(formData.status_go_traveauxR) : null}
-                onChange={newValue => {
-                  handleChange({
-                    target: {
-                      name: 'status_go_traveauxR',
-                      value: newValue ? newValue.format('YYYY-MM-DD') : '',
-                    },
-                  });
-                }}
-                style={{ marginTop: '4px', width: '100%' }}
-              />
-            </LocalizationProvider>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Status</MDTypography>
-            <FormControl component="fieldset">
-              <Switch
-                checked={isActive}
-                onChange={handleToggleActive}
-                name="is_active"
-                inputProps={{ 'aria-label': 'active status' }}
-              />
-              <FormHelperText>{isActive ? 'Active' : 'Inactive'}</FormHelperText>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MDTypography variant="subtitle2">Relance</MDTypography>
-            <FormControl component="fieldset">
-              <Switch
-                checked={isRelance}
-                onChange={handleToggleRelance}
-                name="relance"
-                inputProps={{ 'aria-label': 'relance status' }}
-              />
-              <FormHelperText>{isRelance ? 'Relance' : 'Non Relance'}</FormHelperText>
-            </FormControl>
-          </Grid>
-        </Grid>
+              <MenuItem value="" disabled>
+                -- Choisir l&apos;etat prerequis --
+              </MenuItem>
+              <MenuItem value="Complet">Complet</MenuItem>
+              <MenuItem value="Incomplet">Incomplet</MenuItem>
+            </Select>
+          </FormControl>
+          <MDInput
+            name="numero_DP"
+            value={formData.numero_DP || ''}
+            onChange={handleChange}
+            placeholder="Numero DP"
+            style={{
+              marginBottom: '5px',
+              width: '300px',
+              marginTop: '10px',
+              borderColor: errors.cout_estime ? 'red' : '',
+            }}
+            required
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="ANO certificat tacite"
+              name="ANO_certificat_tacite"
+              value={formData.ANO_certificat_tacite ? dayjs(formData.ANO_certificat_tacite) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'ANO_certificat_tacite',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Arrete opposition"
+              name="arrete_opposition"
+              value={formData.arrete_opposition ? dayjs(formData.arrete_opposition) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'arrete_opposition',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Derniere verification"
+              name="derniere_verification"
+              value={formData.derniere_verification ? dayjs(formData.derniere_verification) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'derniere_verification',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="MJS"
+              name="MJS"
+              value={formData.MJS ? dayjs(formData.MJS) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'MJS',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '300px' }}
+            />
+          </LocalizationProvider>
+          <FormControl
+            fullWidth
+            style={{
+              marginTop: '2px',
+              marginBottom: '2px',
+              width: '320px',
+            }}
+            required
+          >
+            <Select
+              name="plans"
+              value={formData.plans || ''}
+              onChange={handleChange}
+              displayEmpty
+              style={{
+                padding: '10px',
+                fontSize: '14px',
+                borderColor: errors.plans ? 'red' : '',
+              }}
+              required
+            >
+              <MenuItem value="" disabled>
+                -- Choisir le status du plan --
+              </MenuItem>
+              <MenuItem value="OK">OK</MenuItem>
+              <MenuItem value="NOK">NOK</MenuItem>
+            </Select>
+          </FormControl>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Production DP PC"
+              name="production_DP_PC"
+              value={formData.production_DP_PC ? dayjs(formData.production_DP_PC) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'production_DP_PC',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Recipisse depot DP"
+              name="recipisse_depot_DP"
+              value={formData.recipisse_depot_DP ? dayjs(formData.recipisse_depot_DP) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'recipisse_depot_DP',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Status go traveaux (P)"
+              name="status_go_traveauxP"
+              value={formData.status_go_traveauxP ? dayjs(formData.status_go_traveauxP) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'status_go_traveauxP',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Status go traveaux (R)"
+              name="status_go_traveauxR"
+              value={formData.status_go_traveauxR ? dayjs(formData.status_go_traveauxR) : null}
+              onChange={newValue => {
+                handleChange({
+                  target: {
+                    name: 'status_go_traveauxR',
+                    value: newValue ? newValue.format('YYYY-MM-DD') : '',
+                  },
+                });
+              }}
+              style={{ marginBottom: '10px', width: '270px' }}
+            />
+          </LocalizationProvider>
+        </div>
+        <div>
+          <InputLabel>{isActive ? 'Active' : 'Inactive'}</InputLabel>
+          <Switch type="checkbox" checked={isActive} onChange={handleToggleActive}>
+            {' '}
+            {isActive ? 'Active' : 'Inactive'}
+          </Switch>
+          <InputLabel>{isRelance ? 'Relance' : 'Non Relance'}</InputLabel>
+          <Switch type="checkbox" checked={isRelance} onChange={handleToggleRelance}>
+            {isRelance ? 'Relance' : 'Non Relance'}
+          </Switch>
+        </div>
         <div className={styles.buttonContainer}>
           <MDButton onClick={handleSubmit} variant="gradient" color="dark">
             enregistrer
